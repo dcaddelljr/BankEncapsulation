ae6ccbcc9670571c3d88eca5c1e3adfb59e8b66d
Dwight Caddell <dcaddelljr@gmail.com>
2023-03-20 10:25:39 -0500

    BankEncapsulation

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace BankEncapsulation
+{
+	public class BankAccount
+	{
+		public BankAccount()
+		{
+		}
+		private double balance; //only accessible in this class
+		public void Deposit(double depositAmount)
+		{
+			balance += depositAmount;
+		}
+
+		public void Withdraw(double depositAmount)
+		{
+			balance -= depositAmount;
+		}
+		public double GetBalance()
+		{
+			return balance;
+		}
+	}
+}
+
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount();
+
+            account.Deposit(300);
+            account.Withdraw(50);
+            account.GetBalance();
         }
     }
 }
